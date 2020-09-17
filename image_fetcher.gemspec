83513a5839d076eff1b616fbc7c326e13e230b91
Ilkham Gaysin <ilgamgaysin@gmail.com>
2020-09-17 15:18:22 +0300

    Add code climate and badges to readme (#3)
    * Add simplecov and setup sending reports to Code Climate
    
    * Send reports for any branch
    
    * Update Gemfile.lock
    
    * Fix quality
    
    * Enable sending report only for master

--- image_fetcher.gemspec
+++ image_fetcher.gemspec
@@ -38,5 +38,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rubocop'
   spec.add_development_dependency 'rubocop-faker'
   spec.add_development_dependency 'rubocop-rspec'
+  spec.add_development_dependency 'simplecov', '~> 0.17.1'
   spec.add_development_dependency 'vcr'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+if ENV['CI']
+  require 'simplecov'
+
+  SimpleCov.start do
+    command_name "from_container_#{ENV.fetch('CIRCLE_NODE_INDEX', '')}"
+    add_filter %w[bundle spec bin vendor tmp]
+    coverage_dir ENV.fetch('COVERAGE_DIR', '.coverage')
+  end
+end
+
 require 'bundler/setup'
 require 'image_fetcher'
 
